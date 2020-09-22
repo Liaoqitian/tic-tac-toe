2d2d331d70a38c3965689b26b058efdec2bcd7f7
Qitian Liao <liaoqitian1024@gmail.com>
2020-09-22 16:00:45 -0400

    tic tac toe

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.tictactoe;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private Button[][] buttons = new Button[3][3];
     private boolean player1Turn = true;
     private int roundCount;
@@ -23,10 +26,114 @@ public class MainActivity extends AppCompatActivity {
         textViewPlayer1 = findViewById(R.id.text_player1);
         textViewPlayer2 = findViewById(R.id.text_player2);
 
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                String buttonID = "button_" + row + col;
+                int resID = getResources().getIdentifier(buttonID, "id", getPackageName());
+                buttons[row][col] = findViewById(resID);
+                buttons[row][col].setOnClickListener(this);
             }
         }
+
+        Button buttonReset = findViewById(R.id.button_reset);
+        buttonReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                resetGame();
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (! ((Button) view).getText().toString().equals("")) return;
+        if (player1Turn) ((Button) view).setText("X");
+        else ((Button) view).setText("O");
+        roundCount++;
+        if (checkWin()) {
+            if (player1Turn) player1Wins();
+            else player2Wins();
+        } else if (roundCount == 9) draw();
+        else player1Turn = !player1Turn;
+    }
+
+    private boolean checkWin() { // Check whether the game is over
+        String[][] grid = new String[3][3];
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                grid[row][col] = buttons[row][col].getText().toString();
+            }
+        }
+        for (int row = 0; row < 3; row++) {
+            if (grid[row][0].equals("")) continue;
+            if (grid[row][0].equals(grid[row][1]) && grid[row][0].equals(grid[row][2])) return true;
+        }
+        for (int col = 0; col < 3; col++) {
+            if (grid[0][col].equals("")) continue;
+            if (grid[0][col].equals(grid[1][col]) && grid[0][col].equals(grid[2][col])) return true;
+        }
+        if (grid[0][0].equals(grid[1][1]) && grid[1][1].equals(grid[2][2]) && !grid[0][0].equals("")) return true;
+        if (grid[0][2].equals(grid[1][1]) && grid[1][1].equals(grid[2][0]) && !grid[0][2].equals("")) return true;
+        return false;
+    }
+
+    private void player1Wins() {
+        player1Points++;
+        Toast.makeText(this, "Player 1 wins!", Toast.LENGTH_SHORT).show();
+        updatePoints();
+        resetBoard();
+    }
+
+    private void player2Wins() {
+        player2Points++;
+        Toast.makeText(this, "Player 2 wins!", Toast.LENGTH_SHORT).show();
+        updatePoints();
+        resetBoard();
+    }
+
+    private void draw() {
+        Toast.makeText(this, "Draw!", Toast.LENGTH_SHORT).show();
+        resetBoard();
+    }
+
+    private void updatePoints() {
+        textViewPlayer1.setText("Player 1: " + player1Points);
+        textViewPlayer2.setText("Player 2: " + player2Points);
+    }
+
+    private void resetBoard() {
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                buttons[row][col].setText("");
+            }
+        }
+        roundCount = 0;
+        player1Turn = true;
+    }
+
+    private void resetGame() {
+        player1Points = 0;
+        player2Points = 0;
+        resetBoard(); 
+        updatePoints();
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("roundCount", roundCount);
+        outState.putInt("player1Points", player1Points);
+        outState.putInt("player2Points", player2Points);
+        outState.putBoolean("player1Turn", player1Turn);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        roundCount = savedInstanceState.getInt("roundCount");
+        player1Points = savedInstanceState.getInt("player1Points");
+        player1Points = savedInstanceState.getInt("player2Points");
+        player1Turn = savedInstanceState.getBoolean("player1Turn");
+
     }
 }
